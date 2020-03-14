6dc850e6f4a0cbe937a62455a7beb3da34fbda55
Samuel Proulx <samuelproulx26@gmail.com>
2020-03-14 14:11:34 -0400

    re-add firebase (now functionnal)

--- Demonfort/Demonfort/AppDelegate.swift
+++ Demonfort/Demonfort/AppDelegate.swift
@@ -7,16 +7,17 @@
 //
 
 import UIKit
-//import Firebase
+import Firebase
+import FirebaseAnalytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    //var window: UIWindow?
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         //Firebase
-        //FirebaseApp.configure()
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
